e9bae7e641639db8c592f516c18859ea4970e7af
Valerio Battaglia <Batta@valerios-imac.fritz.box>
2014-07-03 03:24:41 +0200

    Better OOP structure Documentation

--- test/test.js
+++ test/test.js
@@ -3,9 +3,9 @@ var chai = require('chai');
 describe('Looper', function() {
   describe('#getNext()', function() {
     it('should return next in array', function() {
-      var looper = require('../lib2.0/looper');
+      var Looper = require('../lib/looper');
 
-      var loopFiles = looper(['a', 'b', 'c']);
+      var loopFiles = new Looper(['a', 'b', 'c']);
       chai.expect(loopFiles.getNext()).to.equal('a');
     });
   });
@@ -14,9 +14,9 @@ describe('Looper', function() {
 describe('Looper', function() {
   describe('#getNext() over all the array', function() {
     it('should return all the elements in order', function() {
-      var looper = require('../lib2.0/looper');
+      var Looper = require('../lib/looper');
 
-      var loopFiles = looper(['a', 'b', 'c']);
+      var loopFiles = new Looper(['a', 'b', 'c']);
       chai.expect(loopFiles.getNext()).to.equal('a');
       chai.expect(loopFiles.getNext()).to.equal('b');
       chai.expect(loopFiles.getNext()).to.equal('c');
@@ -27,9 +27,9 @@ describe('Looper', function() {
 describe('Looper', function() {
   describe('#getNext() with loop', function() {
     it('should return the first on array end', function() {
-      var looper = require('../lib2.0/looper');
+      var Looper = require('../lib/looper');
 
-      var loopFiles = looper(['a', 'b', 'c'], true);
+      var loopFiles = new Looper(['a', 'b', 'c'], true);
       chai.expect(loopFiles.getNext()).to.equal('a');
       chai.expect(loopFiles.getNext()).to.equal('b');
       chai.expect(loopFiles.getNext()).to.equal('c');
